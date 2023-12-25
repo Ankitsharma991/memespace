9e3209245a1434b150193085072c7c51c299dd4a
Ankitsharma991 <sharmaa7861@gmail.com>
2023-12-26 03:05:40 +0530

    started signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import SignupForms from "./_auth/forms/SignupForms";
 
 function App() {
-
   return (
-    <>
-      <p>memespace</p>
-    </>
-  )
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForms />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
